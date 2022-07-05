4d39203b075fe3cfb00127efa27698839f950a3f
Katlegodipitso <katlegosam82@gmail.com>
2022-07-05 12:25:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include <main.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
-*_putchar - writes the character c to stdout
-*@c: The character to print
-*Return: On success 1.
+*main - prints _putchar, flowd by a new line
+*Return: Alwys 0 (Success)
 */
 
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
+char b[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(b[c]);
+}
+_putchar('\n');
+return (0);
 }
